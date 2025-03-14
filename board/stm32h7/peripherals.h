ccd19e5cc94c3169d28dbe6756c64014f76913c8
Robbe Derks <robbe.derks@gmail.com>
2025-03-14 18:01:45 +0100

    Init cleanup: global LED driver (#2170)
    * wip
    
    * fix init
    
    * fix libpanda
    
    * cleanup
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>

--- board/stm32h7/peripherals.h
+++ board/stm32h7/peripherals.h
@@ -38,25 +38,6 @@ void gpio_uart7_init(void) {
 
 // Common GPIO initialization
 void common_init_gpio(void) {
-  /// E2,E3,E4: RGB LED
-  set_gpio_pullup(GPIOE, 2, PULL_NONE);
-  set_gpio_mode(GPIOE, 2, MODE_OUTPUT);
-  set_gpio_output_type(GPIOE, 2, OUTPUT_TYPE_OPEN_DRAIN);
-
-#ifdef HW_RICHIE_REV1
-  set_gpio_pullup(GPIOE, 3, PULL_NONE);
-#else
-  set_gpio_pullup(GPIOE, 3, PULL_NONE);
-#endif
-  set_gpio_mode(GPIOE, 3, MODE_OUTPUT);
-#ifndef HW_RICHIE_REV1
-  set_gpio_output_type(GPIOE, 3, OUTPUT_TYPE_OPEN_DRAIN);
-#endif
-
-  set_gpio_pullup(GPIOE, 4, PULL_NONE);
-  set_gpio_mode(GPIOE, 4, MODE_OUTPUT);
-  set_gpio_output_type(GPIOE, 4, OUTPUT_TYPE_OPEN_DRAIN);
-
 #ifndef RICHIE
   //F11: VOLT_S
   set_gpio_pullup(GPIOF, 11, PULL_NONE);
