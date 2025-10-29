f30d9facf41ef688caef8fb958f10f2384f6a570
Abbas Shah <abbas.shah256@gmail.com>
2025-10-28 21:08:54 -0400

    Add bus entry as parameter in spaming can

--- board/jungle/scripts/spam_can.py
+++ board/jungle/scripts/spam_can.py
@@ -2,26 +2,45 @@
 import os
 import usb1
 import random
+import argparse
 from opendbc.car.structs import CarParams
 from panda import PandaJungle
+
+MAX_BUS_ERROR_CNT = 10
 
 def get_test_string():
   return b"test" + os.urandom(10)
 
 if __name__ == "__main__":
-  p = PandaJungle()
+  parser = argparse.ArgumentParser(description="Read VIN from a vehicle using UDS over CAN.")
+  parser.add_argument('-b', '--bus', type=int, default=None,
+                      help='CAN bus number to spam (default: 0, 1, 2)')
+  args = parser.parse_args()
 
+  buses = [args.bus] if args.bus is not None else [0, 1, 2]
+
+  p = PandaJungle()
   p.set_safety_mode(CarParams.SafetyModel.allOutput)
 
   print("Spamming all buses...")
-  count = 0
+  bus0_count = 0
+  bus1_count = 0
+  bus2_count = 0
+
   while True:
     at = random.randint(1, 2000)
     st = get_test_string()[0:8]
-    bus = random.randint(0, 2)
+    # Choose random from buses
+    bus = random.choice(buses)
     try:
       p.can_send(at, st, bus)
-      count += 1
+      if bus == 0:
+        bus0_count += 1
+      elif bus == 1:
+        bus1_count += 1
+      elif bus == 2:
+        bus2_count += 1
     except usb1.USBErrorTimeout as e:
-      break
-  print(f"Sent {count} messages")
+      pass
+    print(f"Message Counts... Bus 0: {bus0_count} Bus 1: {bus1_count} Bus 2: {bus2_count}", end='\r')
+  print(f"Message Counts... Bus 0: {bus0_count} Bus 1: {bus1_count} Bus 2: {bus2_count}")
