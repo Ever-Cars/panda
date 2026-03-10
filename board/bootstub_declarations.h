6c2d04c497c3531a249453fbbbaf0f804ddb8aa1
Abbas Shah <abbas.shah256@gmail.com>
2026-03-09 22:27:19 -0400

    Mark hexdump with unused attribute

--- board/bootstub_declarations.h
+++ board/bootstub_declarations.h
@@ -3,7 +3,7 @@ void print(const char *a){ UNUSED(a); }
 void puth(unsigned int i){ UNUSED(i); }
 void puth2(unsigned int i){ UNUSED(i); }
 __attribute__((unused)) static void puth4(unsigned int i){ UNUSED(i); }
-void hexdump(const void *a, int l){ UNUSED(a); UNUSED(l); }
+__attribute__((unused)) static void hexdump(const void *a, int l){ UNUSED(a); UNUSED(l); }
 typedef struct board board;
 typedef struct harness_configuration harness_configuration;
 void pwm_init(TIM_TypeDef *TIM, uint8_t channel);
