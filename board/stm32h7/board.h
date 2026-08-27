21ac4c64b4d19861b334e0733e69ee91ad7dc729
Abbas Shah <abbas.shah256@gmail.com>
2026-08-26 20:21:31 -0600

    Richie rev3 is the default board

--- board/stm32h7/board.h
+++ board/stm32h7/board.h
@@ -15,12 +15,9 @@
 #include "board/boards/red.h"
 #include "board/boards/tres.h"
 #include "board/boards/cuatro.h"
-#if defined(RICHIE_REV2)
+#ifdef RICHIE_REV2
   #include "board/boards/richie_rev2.h"
-#elif defined(RICHIE_REV3)
-  #include "board/boards/richie.h"
 #else
-  // Generic H7 builds need this definition for runtime board detection.
   #include "board/boards/richie.h"
 #endif
 
